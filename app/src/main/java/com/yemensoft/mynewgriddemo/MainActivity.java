692b2aa55b0dadcb0acfcb4ff7a551d38f75b921
Waleed <wmonawer.se@gmail.com>
2020-05-19 03:07:40 +0300

    by waleed on 2020-05-19

--- app/src/main/java/com/yemensoft/mynewgriddemo/MainActivity.java
+++ app/src/main/java/com/yemensoft/mynewgriddemo/MainActivity.java
@@ -3,10 +3,12 @@ package com.yemensoft.mynewgriddemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.print.PdfPrint;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.yemensoft.htmlreportslibrary.Activities.HtmlReportViewer;
 import com.yemensoft.htmlreportslibrary.UI.MyTableView;
 import com.yemensoft.htmlreportslibrary.Utilities.ReportModel;
 import com.yemensoft.htmlreportslibrary.Utilities.TableLister;
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tvTitl;
     private MyTableView WebTable;
-    private Button btnAdd;
+    private Button btnAdd,btn_showReport,btn_Share;
 
     ArrayList<ItmModel> mItmModelsList;
 
@@ -41,10 +43,35 @@ public class MainActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                btn_Share.setVisibility(View.GONE);
                 AddItmToGrid();
             }
         });
+
+        btn_showReport=(Button) findViewById(R.id.btn_showReport);
+
+        btn_showReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AddItmToGrid();
+                AddItmToGrid();
+                btn_Share.setVisibility(View.VISIBLE);
+                ShowReport(1,mItmModelsList);
+            }
+        });
+
+        btn_Share =(Button) findViewById(R.id.btn_Share);
+        btn_Share.setVisibility(View.GONE);
+        btn_Share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               shareReport();
+            }
+        });
     }
+
 
     private void AddItmToGrid() {
         if(mItmModelsList==null)
@@ -68,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void ShowDocDtlList(int lng, ArrayList<ItmModel> itmModelArrayList) {
 
-        
 
 
         ReportModel reportModel = new ReportModel();
@@ -146,4 +172,87 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void ShowReport(int lng, ArrayList<ItmModel> itmModelArrayList) {
+
+
+
+        ReportModel reportModel = new ReportModel();
+
+
+        reportModel.langId=lng;
+        reportModel.ReportName="كشف حساب ";
+        reportModel.ReportHtmlFileName="GenReportA4.html";
+        reportModel.largColIndex=1;
+        reportModel.PaperSize=80;
+
+
+
+
+        JSONArray jsonArray = new JSONArray();
+
+
+
+        jsonArray.put("الرصيد الافتتاحي");
+        jsonArray.put("5000 ريال");
+        jsonArray.put("الرصيد الختامي ");
+        jsonArray.put("3000 ريال");
+
+        reportModel.MSTJsonArrayDataSnglRow=jsonArray.toString();
+
+         jsonArray = new JSONArray();
+        jsonArray.put("رقم الصنف");
+        jsonArray.put("اسم الصنف");
+        jsonArray.put("الوحدة");
+        jsonArray.put("الوصف");
+
+
+
+
+        reportModel.DTLTableHeaderJsonArrayDataSnglRow=jsonArray.toString();
+
+
+
+        JSONArray jsonout = new JSONArray();
+
+        if(itmModelArrayList!=null) {
+            for (ItmModel dbitm : itmModelArrayList) {
+
+
+                jsonArray = new JSONArray();
+
+
+
+                jsonArray.put(""+dbitm.itm_no);
+                jsonArray.put(""+dbitm.itm_name);
+                jsonArray.put(dbitm.itm_uint);
+                jsonArray.put(""+dbitm.itm_info);
+
+
+                jsonout.put(jsonArray);
+
+            }
+
+            reportModel.DTLJsonArrayDataMultiRow = jsonout.toString();
+
+            jsonArray = new JSONArray();
+
+            jsonArray.put("اجمالي الاصناف ");
+            jsonArray.put(""+ mItmModelsList.size());
+            reportModel.FooterJsonArrayDataSnglRow=jsonArray.toString();
+
+        }
+
+
+     //   HtmlReportViewer htmlReportViewer=new HtmlReportViewer(this);
+       // htmlReportViewer.Show(reportModel);
+
+        WebTable.ShowReport(reportModel,null);
+
+
+    }
+
+
+    private void shareReport() {
+        WebTable.Share();
+    }
 }
